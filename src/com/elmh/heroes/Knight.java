876bac8e3f0cd6b3956c63d32c8023fe6a7a1dbc
oldvvest <oldvvest@gmail.com>
2014-01-21 23:38:36 +0400

    one class for stats

--- src/com/elmh/heroes/Knight.java
+++ src/com/elmh/heroes/Knight.java
@@ -1,33 +1,16 @@
 package com.elmh.heroes;
 
-public class Knight {
+import com.elmh.CharacterStats;
 
-private static final String NAME = "Рыцарь";
-private int health = 30;
-private int min_att = 4;
-private int max_att = 6;
-private int attack =(int)(min_att + Math.random()*((max_att-min_att)+1));
-private int def = 0;
+public class Knight extends CharacterStats {
 
+    private static final String NAME = "Рыцарь";
+    private static final int HEALTH = 30;
+    private static final int MIN_ATT = 4;
+    private static final int MAX_ATT = 6;
+    private static final int DEF = 0;
 
-    public static String getName() {
-        return NAME;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public int getDef() {
-        return def;
-    }
-
-    public void setAttack(int addAttack) {
-        this.min_att = min_att + addAttack;
-        this.max_att = max_att + addAttack;
+    public Knight() {
+        super(NAME, HEALTH, MAX_ATT, MIN_ATT);
     }
 }
